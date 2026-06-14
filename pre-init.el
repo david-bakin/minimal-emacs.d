4560ee562f804c69df65d9f49665ec2c4b5dfce4
David Bakin <david@bakins-bits.com>
2026-06-13 17:08:58 -0700

    feat: set `source-directory` so package `helpful` can find Emacs sources - C and Elisp
    Signed-off-by: David Bakin <david@bakins-bits.com>

--- pre-init.el
+++ pre-init.el
@@ -1,1 +1,4 @@
 ;;; pre-init.el --- run by minimal-emacs.d _before_ init.el -*- no-byte-compile: t; lexical-binding: t; -*-
+
+;; (I moved the sources since I build emacs - this will help package helpful find them))
+(setq source-directory "C:/Drives/Repos/my-tools/emacs/emacs/")
